a6b7eacc8e7fc6703966836377c4414f7c5a4377
Denzel-R <denzel_R@mail.ru>
2025-10-26 02:14:23 +0300

    add JQuery fix slider

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
-const carousel = document.querySelector('.reviews-scroll');
-const leftbutton = document.querySelector('.arrow-left');
-const rightbutton = document.querySelector('.arrow-right');
+$(document).ready(function() {
+  const $carousel = $('.reviews-scroll');
+  const scrollAmount = 300;
 
-leftbutton.addEventListener('click', () => {
-  carousel.scrollBy({
-    left: -130,
+  $('.arrow-left').on('click', function() {
+    $carousel.animate({
+      scrollLeft: $carousel.scrollLeft() + scrollAmount
+    }, 100);
+  });
+  $('.arrow-right').on('click', function() {
+    $carousel.animate({
+      scrollLeft: $carousel.scrollLeft() - scrollAmount
+    }, 100);
   });
 });
-rightbutton.addEventListener('click', () => {
-  carousel.scrollBy({
-    left: 130,
-  });
-});
-const carousel1 = document.querySelector('.tariffs-scroll');
-const leftbutton1 = document.querySelector('.left-arrow');
-const rightbutton1 = document.querySelector('.right-arrow');
 
-leftbutton1.addEventListener('click', () => {
-  carousel1.scrollBy({
-    left: -260,
+$(document).ready(function() {
+  const $carousel = $('.tariffs-scroll');
+  const scrollAmount = 500;
+
+  $('.left-arrow').on('click', function() {
+    $carousel.animate({
+      scrollLeft: $carousel.scrollLeft() + scrollAmount
+    }, 100);
   });
-});
-rightbutton1.addEventListener('click', () => {
-  carousel1.scrollBy({
-    left: 260,
+
+  $('.right-arrow').on('click', function() {
+    $carousel.animate({
+      scrollLeft: $carousel.scrollLeft() - scrollAmount
+    }, 100);
   });
 });
